d3152de283d5ef247da9349b5fca8f3d5da331b8
eduardo1590 <eduardo1590@gmail.com>
2017-09-11 15:59:55 -0400

    Inicio, estructura, htaccess, autoload, request, enrutador

---
+++ Config/Enrutador.php
@@ -0,0 +1,32 @@
+<?php namespace Config;
+
+	//ESTE ARCHIVO EN LOS FRAMEWORKS SUELE LLAMARSE BOOTSTRAP
+	class Enrutador{
+
+		public static function run(Request $request){
+			$controlador = $request->getControlador() . "Controller";
+			$ruta = ROOT . "Controllers" . DS .  $controlador .".php";
+			$metodo = $request->getMetodo();
+			if($metodo == "index.php"){
+				$metodo = "index";
+			}
+			$argumento = $request->getArgumento();
+			if(is_readable($ruta)){
+				require_once $ruta;
+				$mostrar = "Controllers\\" . $controlador;
+				$controlador = new $mostrar;
+				if(!isset($metodo)){
+					call_user_func(array($controlador, $metodo));
+				}else{
+					call_user_func_array(array($controlador, $metodo), $argumento);
+				}
+			}
+		}
+	}
+	
+	if(isset($_GET['url'])){
+		$url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL);
+		$url = explode('/', $url);
+		$url = array_filter($url);
+	}
+?>
